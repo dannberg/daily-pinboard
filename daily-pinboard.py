08f13efa22d59f22fd37f07542f7e58a74433738
Dann Berg <dann@dann-berg.com>
2022-12-04 10:23:46 -0500

    start code for adding list data to email

--- daily-pinboard.py
+++ daily-pinboard.py
@@ -1,6 +1,10 @@
 import pinboard
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
+import smtplib
+from email.message import EmailMessage
+
+# A special thank you to OpenAI's ChatGCP for the code assistance!
 
 pb = pinboard.Pinboard('dannberg:351F526D563F33F2F9FC')
 
@@ -20,3 +24,28 @@ for x in range(0, numOfYears):
     post = pb.posts.all(start=0, results=20, fromdt=dayBeforeSearchDate, todt=searchDate)
     if post:
         datePosts.append(post)
+
+# Get current month and day as strings
+current_date = datetime.now()
+current_month = current_date.strftime('%B')
+current_day = current_date.strftime('%d')
+
+# Set up the email message
+msg = EmailMessage()
+msg['Subject'] = 'Pinboard Posts for ' + current_month + ' ' + current_day
+msg['From'] = 'dann@dannberg.me'
+msg['To'] = 'dann@dannb.org'
+
+# Set email message body as content of datePosts array
+# this doesn't work yet. datePosts is type `list`.
+# Need to figure out how to add the year before each post, and then get all the text data out of the inner lists
+email_body = ''
+for inner_array in datePosts:
+  email_body += '\n'.join(inner_array) + '\n'
+
+# Set up the SMTP server and send the email
+server = smtplib.SMTP('dannberg.me')
+server.starttls()
+server.login('dann@dannberg.me', 'glyiC)jac@vaSt%ul:')
+server.send_message(msg)
+server.quit()
