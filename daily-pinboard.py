399c8d5f2344d4d5f7b2fd2ff9a91b6d225b4503
Dann Berg <dann@dann-berg.com>
2022-12-04 09:24:20 -0500

    add comments

--- daily-pinboard.py
+++ daily-pinboard.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 pb = pinboard.Pinboard('dannberg:351F526D563F33F2F9FC')
+
+# Manually look to see what year you had your first Pinboard post
 firstPostYear = 2012
 
 numOfYears = datetime.now().year - firstPostYear + 1
@@ -11,6 +13,7 @@ year = datetime.now() - relativedelta(years=1)
 
 datePosts = []
 
+# Loops through today's date for each year of Pinboard posts, if a post exists, adds it to datePosts array
 for x in range(0, numOfYears):
     searchDate = datetime.now() - relativedelta(years=x)
     dayBeforeSearchDate = searchDate - timedelta(days=1)
