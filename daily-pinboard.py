4899ff6a55afdf3c477bebb565f04af7f386bbc8
Dann Berg <dann@dann-berg.com>
2022-12-05 09:52:30 -0500

    replace secrets with config variables

--- daily-pinboard.py
+++ daily-pinboard.py
@@ -1,4 +1,5 @@
 import pinboard
+import config
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 import smtplib
@@ -6,7 +7,7 @@ from email.message import EmailMessage
 
 # A special thank you to OpenAI's ChatGCP for the code assistance!
 
-pb = pinboard.Pinboard('dannberg:351F526D563F33F2F9FC')
+pb = pinboard.Pinboard(config.PINBOARD_API_TOKEN)
 
 # Manually look to see what year you had your first Pinboard post
 firstPostYear = 2012
@@ -23,9 +24,6 @@ for x in range(0, numOfYears):
     searchDate = datetime.now() - relativedelta(years=x)
     dayBeforeSearchDate = searchDate - timedelta(days=1)
     post = pb.posts.all(start=0, results=20, fromdt=dayBeforeSearchDate, todt=searchDate)
-    # if post:
-    #     datePosts.append(searchDate.year) # possibly remove?
-    #     datePosts.append(post)
     if post:
         year_str = str(searchDate.year)
         email_body += f"Year: {year_str}\n"
@@ -43,25 +41,15 @@ current_day = current_date.strftime('%d')
 # Set up the email message
 msg = EmailMessage()
 msg['Subject'] = 'Pinboard Posts for ' + current_month + ' ' + current_day
-msg['From'] = 'dann@dannberg.me'
-msg['To'] = 'dann@dannb.org'
+msg['From'] = config.MSG_FROM
+msg['To'] = config.MSG_TO
 
 # Set email message body as content of datePosts array
-
-
-# for sublist in datePosts:
-#     # Iterate over the Bookmark objects in the sub-list
-#     for bookmark in sublist:
-#         # Extract the description and url and add them to the email body string
-#         description = bookmark.description
-#         url = bookmark.url
-#         email_body += f"{description}: {url}\n"
-
 msg.set_content(email_body)
 
 # Set up the SMTP server and send the email
-server = smtplib.SMTP('dannberg.me')
+server = smtplib.SMTP(config.SMTP_SERVER)
 server.starttls()
-server.login('dann@dannberg.me', 'glyiC)jac@vaSt%ul:')
+server.login(config.SMTP_USERNAME, config.SMTP_PASS)
 server.send_message(msg)
 server.quit()
